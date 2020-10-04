f425b10afd6fb655b38761a39cbe739041862792
AyushRawat-17 <ayushrawatkv@gmail.com>
2020-10-04 22:45:41 +0530

    Home Page Done

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>&copy; 2020 Ayush Rawat</p>
+            <p>Privacy . Terms . Sitemap . Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
